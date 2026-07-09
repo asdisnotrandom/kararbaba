322013c8f1e2ee6fc00619d8f08ca38755efdcff
asdisnotrandom <demirelahme123@gmail.com>
2026-07-09 08:00:00 +0300

    lidarsonayakin

--- pi4_baba/src/sensorler/rplidars3.rs
+++ pi4_baba/src/sensorler/rplidars3.rs
@@ -3,6 +3,7 @@ use crate::veri_tipleri::{LidarNokta, LidarVeri};
 use tokio::sync::mpsc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio_serial::SerialPortBuilderExt;
+use std::time::{UNIX_EPOCH, SystemTime};
 
 const LIDAR_PORT: &str = "amanin";
 const LIDAR_BAUD: u32 = 115200;
@@ -22,10 +23,35 @@ pub async fn lidar_task(tx: mpsc::Sender<LidarVeri>)
         eprintln!("Response yanlis: {:?}", e);
         return;
     }
-    let mut buf: [u8; 5] = [0u8; 5];
+    let mut buf: [u8; 5] = [0u8; 5];{
     let mut bufid = 0;
     let mut anlik_tur: Vec<LidarNokta> = Vec::with_capacity(1500);
     loop {
-        
+        if let Ok(_) = port.write_all(&mut buf[bufid..bufid+1]).await
+        {
+            bufid += 1;
+            if bufid == 5
+            {
+                let check1 = buf[0] & 0x01;
+                let check2 = (buf[0] >> 1) & 0x01;
+                let check3 = buf[1] & 0x01;
+                if check1 != check2 && check3 == 1
+                {
+                    let start_flag = 1;
+                    if start_flag && !anlik_tur.is_empty()
+                    {
+                        let simdiki_zaman = SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_millis() as u64;
+                    }
+                }
+                else {
+                    buf.copy_within(1..5, 0 );
+                    bufid = 4;
+                }
+            }
+        }
+        }
     } 
 }
